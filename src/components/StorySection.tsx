614ab43c26209ea07431e124aa148522aa9ee8cb
wigmi7 <wigmi7@gmail.com>
2026-03-25 12:29:28 -0600

    Add files via upload

--- src/components/StorySection.tsx
+++ src/components/StorySection.tsx
@@ -18,7 +18,7 @@ export default function StorySection() {
           </div>
           
           <div className="story-image-block cinematic-zoom reveal-up active" style={{ animationDelay: '0.2s' }}>
-            <img src="https://images.unsplash.com/photo-1502086223501-7ea6ecd79368?auto=format&fit=crop&q=80&w=1200" alt="Vista del Volcán y Laguna de Ayarza" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Lake_Atitlan_Guatemala.jpg/1280px-Lake_Atitlan_Guatemala.jpg" alt="Vista del Volcán y Laguna de Ayarza" />
           </div>
 
           <div className="story-image-block cinematic-zoom reveal-up active" style={{ animationDelay: '0.3s' }}>
